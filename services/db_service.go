c1225a22729b21feeeaed7d883eadf7999b14a76
Gabriel Fernandes <force2606@gmail.com>
2022-05-16 14:11:42 +0100

    Deposit Table Migration

--- services/db_service.go
+++ services/db_service.go
@@ -24,7 +24,7 @@ func Setup() {
 	log.Println("Established connection to Database")
 
 	/* Create tables that dont already exist */
-	migErr := Database.AutoMigrate(&models.OilBin{}, &models.User{}, &models.Bottle{}, &models.Sensor{}, &models.SensorReading{})
+	migErr := Database.AutoMigrate(&models.OilBin{}, &models.User{}, &models.Bottle{}, &models.Sensor{}, &models.SensorReading{}, &models.Deposit{})
 	if migErr != nil {
 		panic(migErr)
 	}
